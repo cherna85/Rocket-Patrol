549f8e84a8af4eec3a82945024856e7a26f37b46
Citlalli Hernandez <cherna85@ucsc.edu>
2022-04-09 23:10:25 -0700

    spaceship reset and animation added

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -11,7 +11,11 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= this.moveSpeed;
         //wrap edges
         if(this.x <= 0 - this.width){
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    reset(){
+        this.x = game.config.width;
     }
 }
